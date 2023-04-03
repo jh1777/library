d62ca0a7bf355a1db488a90a587e3dcefea37b8b
Joerg (MacBookPro) <jh.stores1@gmail.com>
2023-04-03 20:23:32 +0200

    tests

--- projects/ui-lib/src/lib/components/tags/tags.component.cy.ts
+++ projects/ui-lib/src/lib/components/tags/tags.component.cy.ts
@@ -1,3 +1,4 @@
+import { createOutputSpy } from "cypress/angular";
 import { TagsComponent } from "./tags.component";
 
 describe('TagsComponent', () => {
@@ -38,6 +39,40 @@ describe('TagsComponent', () => {
 
         cy.get('.csgp-tags-group > :nth-child(2)').should('not.contain.text', 'K1');
         cy.get('.csgp-tags-group > :nth-child(2)').should('have.class', 'pulse');
+        
+    });
+
+    it('Clicked check', () => {
+        cy.mount(TagsComponent, {
+            componentProperties: {
+                tags: [
+                    {
+                        description: "Test #1",
+                        name: "K1",
+                        value: "V1"
+                    },
+                    {
+                        description: "Test #2",
+                        name: "K2",
+                        value: "V2"
+                    }
+                ],
+                isLoading: false,
+                enableClick: true,
+                enableClickMore: true,
+                overflowAfterXItems: 1,
+                //onMoreClick: createOutputSpy('moreClickedSpy'),
+                onClick: createOutputSpy('tagClickedSpy')
+            }
+        });
+
+        cy.get('[title="Test #1"]').realClick();
+        cy.get('@tagClickedSpy').should('have.been.calledOnce');
+
+       // cy.get('.cy-csgp-tag-more').click();
+       // cy.get('@moreClickedSpy').should('have.been.called');
+
+        
         
     })
 })
