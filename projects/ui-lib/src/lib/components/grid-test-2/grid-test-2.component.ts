c6ac5237907e3e72e5bc9a30dfc64fc24a8b163f
Joerg (M1) <jh.stores1@gmail.com>
2023-05-04 14:06:33 +0200

    test

--- projects/ui-lib/src/lib/components/grid-test-2/grid-test-2.component.ts
+++ projects/ui-lib/src/lib/components/grid-test-2/grid-test-2.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, ChangeDetectionStrategy, Component, ContentChildren, QueryList } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ContentChildren, Input, QueryList } from '@angular/core';
 import { PropertyEntryComponent } from '../property-entry';
 
 @Component({
@@ -7,53 +7,13 @@ import { PropertyEntryComponent } from '../property-entry';
   styleUrls: ['./grid-test-2.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-/*
-export class GridTestComponent2 implements AfterContentInit {
 
-  @ContentChildren(PropertyEntryComponent) items: QueryList<any>;
-
-  ngAfterContentInit() {
-
-    console.log(this.items);
-
-    this.items.forEach(item => {
-      
-      console.log(item.data);
-    });
-  
-  }
-
-}
-*/
-
-export class GridTestComponent2 implements AfterViewInit {
+export class GridTestComponent2  {
   @ContentChildren(PropertyEntryComponent) children: QueryList<PropertyEntryComponent>;
 
-  rows: PropertyEntryComponent[][] = [];
+  @Input()
+  cols: number;
 
-  ngAfterViewInit() {
-    this.layoutChildren();
-  }
-
-  private layoutChildren() {
-    this.rows = [];
-
-    const children = this.children.toArray();
-
-    const numColumns = 3; // set the number of columns here
-    const numRows = Math.ceil(children.length / numColumns);
-
-    for (let i = 0; i < numRows; i++) {
-      const row = [];
-
-      for (let j = 0; j < numColumns; j++) {
-        const child = children[i * numColumns + j];
-        if (child) {
-          row.push(child);
-        }
-      }
-
-      this.rows.push(row);
-    }
-  }
+  @Input()
+  rows: number;
 }
