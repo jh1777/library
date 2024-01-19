d6338006d7b4768f6dc359682b65dd4a7ba04c4a
Joerg (MacStudio) <jh.stores1@gmail.com>
2024-01-19 23:52:22 +0100

    added states and item icon

--- projects/ui-lib/src/lib/components/entry-tile/entry-tile.component.ts
+++ projects/ui-lib/src/lib/components/entry-tile/entry-tile.component.ts
@@ -12,6 +12,7 @@ export class EntryTileComponent implements OnInit {
 
   constructor() { 
     this.data.title = "Onboarding";
+    this.data.state = EntryState.none;
     this.data.headerProperties.push({
       label: "State",
       value: "Started"
@@ -19,26 +20,41 @@ export class EntryTileComponent implements OnInit {
 
     this.data.items.push({
       primaryValue: "TLS Registration",
-      secondaryValue: "23/22/2023 12:34:43 GMT+1"
+      secondaryValue: "23/22/2023 12:34:43 GMT+1",
+      state: EntryState.attention
     });
 
     this.data.items.push({
       title: "IoT Setup",
       primaryValue: "Successful",
       clickable: true,
+      icon: new IconModel({
+        iconName: "success-standard",
+        color: "green",
+        tooltip: "More Information",
+        size: 30,
+        isClickable: true
+      }),
       secondaryValue: "23/22/2023 12:35:22 GMT+1"
     });
   }
 
   ngOnInit(): void {
+    console.log(this.data);
   }
 
+}
+
+export enum EntryState {
+  none = 0,
+  attention = 1,
+  error = 2
 }
 
 export class EntryTile {
   isLoading: boolean = false;
   title: string = "";
-  state: 'none' | 'attention' | 'error' = 'none';
+  state: EntryState = EntryState.none;
   headerProperties: Array<EntryTileProperty> = [];
   items: Array<EntryTileItem> = [];
 }
@@ -52,6 +68,7 @@ export class EntryTileProperty {
 
 export class EntryTileItem {
   title?: string = "";
+  state?: EntryState = EntryState.none;
   clickable?: boolean = false;
   primaryValue: string = "";
   secondaryValue?: string;
