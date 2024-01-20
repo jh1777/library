74c8135a1951a663ce33996b4c990609cc59aa11
Joerg (MacStudio) <jh.stores1@gmail.com>
2024-01-20 23:02:51 +0100

    several model and design updates

--- projects/ui-lib/src/lib/components/entry-tile/entry-tile.component.ts
+++ projects/ui-lib/src/lib/components/entry-tile/entry-tile.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { IconModel } from '../../models/shared/icon-model';
 
 @Component({
@@ -7,13 +7,26 @@ import { IconModel } from '../../models/shared/icon-model';
   styleUrls: ['./entry-tile.component.scss']
 })
 export class EntryTileComponent implements OnInit {
+  public placeholder = "⏹⏹ ";
+  @Input()
+  public isLoading: boolean = false;
+
+  private _itemLimit: number = 7;
+  public get getItemLimit(): number {
+    return this._itemLimit;
+  }
 
   public data: EntryTile = new EntryTile();
 
+  // Just placeholder for test
+  public errorData;
+
   constructor() { 
     this.data.title = "Onboarding";
+    this.data.collapseMode = EntryTileCollapseMode.manual;
     this.data.state = EntryState.none;
-    this.data.headerProperties.push({
+    // this.data.titleIcon = "factory";
+    this.data.header.push({
       label: "State",
       value: "Started",
       valueIcon: new IconModel({
@@ -35,7 +48,7 @@ export class EntryTileComponent implements OnInit {
         color: "orange",
         tooltip: "No TLS arrived yet, please check with crew!",
         size: 30,
-        isClickable: true
+        isClickable: false
       }),
     });
 
@@ -49,19 +62,71 @@ export class EntryTileComponent implements OnInit {
         color: "green",
         tooltip: "Everything is ready on IoT Hub!",
         size: 30,
-        isClickable: true
+        isClickable: false
       }),
       secondaryValue: "23/22/2023 12:35:22 GMT+1"
     });
   }
 
   ngOnInit(): void {
-    console.log(this.data);
+
   }
 
   public toggleCollapsedState() {
     this.data.isCollapsed = !this.data.isCollapsed;
   }
+
+  // Actions
+  public tileItemClicked($item: EntryTileItem) {
+    if ($item.clickable) {
+      // callback output
+      console.log($item);
+    }
+  }
+
+
+}
+
+
+
+/**
+ * Main class for Entry Tile
+ */
+export class EntryTile {
+  // states
+  isLoading: boolean = false; // done with Input but not in model
+  isCollapsed: boolean = false; // really needed in model?
+  state: EntryState = EntryState.none;
+  collapseMode: EntryTileCollapseMode = EntryTileCollapseMode.manual;
+  // Title
+  title: string = "";
+  titleIcon?: string; // Not recommended
+  // Header
+  header: Array<EntryTileProperty> = [];
+  // Content
+  items: Array<EntryTileItem> = [];
+}
+
+/**
+ * Entry Tile Item class
+ */
+export class EntryTileItem {
+  title?: string = "";
+  state?: EntryState = EntryState.none;
+  clickable?: boolean = false;
+  primaryValue: string = "";
+  secondaryValue?: string;
+  icon?: IconModel;
+}
+
+/**
+ * Generic Label / Value pair class
+ */
+export class EntryTileProperty {
+  label: string;
+  value: string;
+  valueIcon?: IconModel;
+  valueStyle?: string;
 }
 
 export enum EntryState {
@@ -71,37 +136,11 @@ export enum EntryState {
   success = 3
 }
 
-export enum EntryTileViewMode {
+export enum EntryTileCollapseMode {
   // All items shown by default, no collapse button
   disabled = 0,
   /// All items shown by default, but collapse button shown
   manual = 1,
   /// Only attention and error items shown
   autoexpanded = 2  
-}
-
-export class EntryTile {
-  isLoading: boolean = false;
-  title: string = "";
-  state: EntryState = EntryState.none;
-  viewMode: EntryTileViewMode = EntryTileViewMode.manual; // TODO
-  headerProperties: Array<EntryTileProperty> = [];
-  items: Array<EntryTileItem> = [];
-  isCollapsed: boolean = false;
-}
-
-export class EntryTileProperty {
-  label: string;
-  value: string;
-  valueIcon?: IconModel;
-  valueStyle?: string;
-}
-
-export class EntryTileItem {
-  title?: string = "";
-  state?: EntryState = EntryState.none;
-  clickable?: boolean = false;
-  primaryValue: string = "";
-  secondaryValue?: string;
-  icon?: IconModel;
 }
