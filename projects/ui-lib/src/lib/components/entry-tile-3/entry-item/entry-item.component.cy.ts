4a249156c1ba8174c302e22ec8d24fa9c49e3e86
Joerg Hannemann <hannemj@tbdir.net>
2024-04-04 18:19:47 +0200

    updated

--- projects/ui-lib/src/lib/components/entry-tile-3/entry-item/entry-item.component.cy.ts
+++ projects/ui-lib/src/lib/components/entry-tile-3/entry-item/entry-item.component.cy.ts
@@ -5,7 +5,6 @@ import { input, signal } from "@angular/core";
 describe('EntryItemComponent', () => {
   
     beforeEach(() => {
-      var storeReference;
       cy.mount(EntryItemComponent, {
           componentProperties: {
             onItemClick: createOutputSpy('itemClickedSpy'),
@@ -17,5 +16,18 @@ describe('EntryItemComponent', () => {
       }).then((wrapper) => {
         return cy.wrap(wrapper).as("angular");
       })
+    });
+});
+
+it('Loading check', () => {
+    cy.mount(EntryItemComponent, {
+      componentProperties: {
+        isLoading: input(true)
+      }
+    }).then((wrapper) => {
+      return cy.wrap(wrapper).as("angular2");
+    })
+    cy.get("@angular2").then((wrapper) => {
+        cy.get('.csgp-entry-tile-header > :nth-child(1)').should('contain.text', "⏹⏹");
     });
 });
