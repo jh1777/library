33a51e65d6281b9f02801781089630bfdc81ced7
Joerg Hannemann <hannemj@tbdir.net>
2024-04-04 18:11:01 +0200

    re-setuped storybook for ui-lib only

--- projects/ui-lib/src/lib/components/button-v2/button-v2.stories.ts
+++ projects/ui-lib/src/lib/components/button-v2/button-v2.stories.ts
@@ -5,7 +5,7 @@ import { input, signal } from '@angular/core';
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories
 const meta: Meta<ButtonV2Component> = {
-  title: 'Example Button',
+  title: 'UILib/Button',
   component: ButtonV2Component,
   tags: ['autodocs'],
   // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
@@ -14,7 +14,7 @@ const meta: Meta<ButtonV2Component> = {
 
 export default meta;
 type Story = StoryObj<ButtonV2Component>;
-/*
+
 export const Plain: Story = {
   args: {
     label: input('Button'),
@@ -24,4 +24,3 @@ export const Plain: Story = {
     hidden: signal(false)
   },
 };
-*/
