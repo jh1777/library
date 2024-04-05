30511d81703442779fa2b01ce98fefdbbbd71147
Joerg (MacStudio) <jh.stores1@gmail.com>
2024-04-05 22:39:08 +0200

    card content select tryouts

--- projects/ui-lib/src/lib/components/card/card.component.ts
+++ projects/ui-lib/src/lib/components/card/card.component.ts
@@ -1,4 +1,4 @@
-import { AfterContentInit, AfterViewInit, Component, ContentChildren, QueryList, input, signal } from '@angular/core';
+import { AfterContentInit, AfterViewInit, ChangeDetectionStrategy, Component, ContentChildren, QueryList, input, signal } from '@angular/core';
 import { UIBaseComponent } from '../../base/ui-base.component';
 import { ButtonV2Component } from '../button-v2/button-v2.component';
 
@@ -6,6 +6,7 @@ import { ButtonV2Component } from '../button-v2/button-v2.component';
   selector: 'ui-card',
   standalone: true,
   imports: [],
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss'
 })
@@ -14,9 +15,12 @@ export class CardComponent extends UIBaseComponent implements AfterContentInit {
   @ContentChildren(ButtonV2Component) buttons: QueryList<ButtonV2Component>;
 
   ngAfterContentInit(): void {
+    if (this.buttons.length > 2) {
+      throw Error("Maximum number of buttons in a card is 3");
+    }
     for (let index = 0; index < this.buttons.length; index++) {
       const element = this.buttons.get(index);
-      if (index > 1) {
+      if (index > 2) {
         element.hidden.set(true);
       }
     }
