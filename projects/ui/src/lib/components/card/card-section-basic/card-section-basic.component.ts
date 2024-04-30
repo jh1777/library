f551b25ac023cf8fab762d6504443acfb3ca5da6
Joerg (MacStudio) <jh.stores1@gmail.com>
2024-04-30 20:30:58 +0200

    migrate to jest; added card-section

---
+++ projects/ui/src/lib/components/card/card-section-basic/card-section-basic.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-card-section-basic',
+  standalone: true,
+  imports: [],
+  templateUrl: './card-section-basic.component.html',
+  styleUrl: './card-section-basic.component.css'
+})
+export class CardSectionBasicComponent {
+
+}
