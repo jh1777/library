487d10ea0dccc5792043e66ddfc5be55636e84a4
Joerg (MacStudio) <jh.stores1@gmail.com>
2024-07-06 14:55:49 +0200

    add deletion button

--- projects/ui/src/lib/components/navigation/navigation-section/navigation-section.component.ts
+++ projects/ui/src/lib/components/navigation/navigation-section/navigation-section.component.ts
@@ -1,6 +1,12 @@
 import { AfterViewInit, ChangeDetectionStrategy, Component, computed, ContentChildren, input, QueryList } from '@angular/core';
 import { UIBaseComponent } from '../../../shared';
 import { NavigationSectionItemComponent } from './navigation-section-item/navigation-section-item.component';
+import { ClarityModule } from '@clr/angular';
+import {
+  ClarityIcons,
+  trashIcon
+} from "@cds/core/icon";
+import '@cds/core/icon/register.js';
 
 interface NavigationSectionItemExchange {
   label: string;
@@ -12,12 +18,17 @@ interface NavigationSectionItemExchange {
   selector: 'ui-navigation-section',
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [],
+  imports: [ClarityModule],
   templateUrl: './navigation-section.component.html',
   styleUrl: './navigation-section.component.scss'
 })
 export class NavigationSectionComponent extends UIBaseComponent implements AfterViewInit {
   @ContentChildren(NavigationSectionItemComponent) items: QueryList<NavigationSectionItemComponent>;
+
+  constructor() {
+    super()
+    ClarityIcons.addIcons(trashIcon);
+  }
 
   sectionItems = computed(()=> {
     let result: Array<NavigationSectionItemExchange> = [];
@@ -37,9 +48,14 @@ export class NavigationSectionComponent extends UIBaseComponent implements After
     
   }
 
-  itemClicked(item: NavigationSectionItemExchange): void {
+  itemClicked($event: MouseEvent, item: NavigationSectionItemExchange): void {
     this.items.get(item.index).itemClicked.emit();
   }
 
+  itemRemoveClicked($event: MouseEvent, item: NavigationSectionItemExchange): void {
+    $event.preventDefault();
+    this.items.get(item.index).deleteItemClicked.emit();
+  }
+  
   label = input.required<string>();
 }
