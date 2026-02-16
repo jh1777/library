e8deff302ccb207fb916757eaaffc8444c19169f
Joerg (MacAir) <jh.stores1@gmail.com>
2026-02-16 20:29:23 +0100

    reformatting

--- src/app/pages/ui-lib-page/ui-lib-page.component.ts
+++ src/app/pages/ui-lib-page/ui-lib-page.component.ts
@@ -1,59 +1,123 @@
-
-
-import { AfterContentInit, Component, OnInit, ViewChild, signal } from "@angular/core";
-import { BadgeComponent, ButtonComponent, ModalComponent, ConfirmationModalComponent, CardComponent, EntryTileComponent, TabComponent, TabsComponent, CardSectionBasicComponent, EntryKeyValueComponent, EntryTileItemComponent, SwitchComponent, MenuBarComponent, SignpostComponent, EntryContainerComponent, EntryMetricComponent, MenuItemComponent, AccordionComponent, AccordionPanelComponent, AccordionPanelHeaderComponent, ToolbarComponent, ValueTileComponent, GridComponent, MetricTileComponent, ButtonGroupComponent, SwitchButtonComponent, SwitchButtonOptionComponent, SideMenuComponent, SideMenuEntryComponent, SideMenuSectionComponent } from "../../../../projects/ui/src/public-api";
-
- import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
-import { faPlay, faStop, faCircleCheck, faCircleExclamation, faInfoCircle, faTriangleExclamation, faTrash, faExternalLink, faCopy, faTimes } from '@fortawesome/free-solid-svg-icons';
+import { Component, ViewChild, signal } from '@angular/core';
+import {
+  BadgeComponent,
+  ButtonComponent,
+  ModalComponent,
+  ConfirmationModalComponent,
+  CardComponent,
+  EntryTileComponent,
+  TabComponent,
+  TabsComponent,
+  CardSectionBasicComponent,
+  EntryKeyValueComponent,
+  EntryTileItemComponent,
+  SwitchComponent,
+  MenuBarComponent,
+  SignpostComponent,
+  EntryContainerComponent,
+  EntryMetricComponent,
+  MenuItemComponent,
+  AccordionComponent,
+  AccordionPanelComponent,
+  AccordionPanelHeaderComponent,
+  ToolbarComponent,
+  ValueTileComponent,
+  GridComponent,
+  MetricTileComponent,
+  ButtonGroupComponent,
+  SwitchButtonComponent,
+  SwitchButtonOptionComponent,
+  SideMenuComponent,
+  SideMenuEntryComponent,
+  SideMenuSectionComponent,
+} from '../../../../projects/ui/src/public-api';
+import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
+import {
+  faPlay,
+  faStop,
+  faCircleCheck,
+  faCircleExclamation,
+  faInfoCircle,
+  faTriangleExclamation,
+  faTrash,
+  faExternalLink,
+  faCopy,
+  faTimes,
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
-    selector: 'ui-lib-page',
-    standalone: true,
-    imports: [ConfirmationModalComponent, SignpostComponent, BadgeComponent, SwitchComponent, 
-      ModalComponent, 
-      AccordionComponent, AccordionPanelComponent, AccordionPanelHeaderComponent, CardSectionBasicComponent, SideMenuComponent, SideMenuEntryComponent, SideMenuSectionComponent,
-      MenuItemComponent, MenuBarComponent, TabComponent, TabsComponent, GridComponent, ValueTileComponent, SwitchButtonComponent, SwitchButtonOptionComponent,
-      MetricTileComponent, ToolbarComponent, ButtonComponent, CardComponent, EntryTileComponent, EntryKeyValueComponent, 
-      EntryTileItemComponent, EntryContainerComponent, EntryMetricComponent, ButtonGroupComponent],
-    templateUrl: './ui-lib-page.component.html',
-    styleUrls: ['./ui-lib-page.component.scss']
-  })
-  export class UiLibPageComponent  {
+  selector: 'ui-lib-page',
+  standalone: true,
+  imports: [
+    ConfirmationModalComponent,
+    SignpostComponent,
+    BadgeComponent,
+    SwitchComponent,
+    ModalComponent,
+    AccordionComponent,
+    AccordionPanelComponent,
+    AccordionPanelHeaderComponent,
+    CardSectionBasicComponent,
+    SideMenuComponent,
+    SideMenuEntryComponent,
+    SideMenuSectionComponent,
+    TabComponent,
+    TabsComponent,
+    ValueTileComponent,
+    SwitchButtonComponent,
+    SwitchButtonOptionComponent,
+    MetricTileComponent,
+    ToolbarComponent,
+    ButtonComponent,
+    CardComponent,
+    EntryTileComponent,
+    EntryKeyValueComponent,
+    EntryTileItemComponent,
+    EntryContainerComponent,
+    EntryMetricComponent,
+    ButtonGroupComponent,
+  ],
+  templateUrl: './ui-lib-page.component.html',
+  styleUrls: ['./ui-lib-page.component.scss'],
+})
+export class UiLibPageComponent {
+  updateSideMenuSelection(event: any) {
+    console.log('Side Menu Selection Changed:', event);
+  }
 
-    updateSideMenuSelection(event: any) {
-      console.log('Side Menu Selection Changed:', event);
-    }
+  sideMenuSelectedValue = signal<string | number | boolean>('list');
 
-    sideMenuSelectedValue = signal<string | number | boolean>('list');
+  selectedItemsDebug = signal<string[]>([]);
+  selectedItemsUpdate($event: string[]) {
+    this.selectedItemsDebug.set($event);
+    console.log('Selected Items Updated:', $event);
+  }
+  @ViewChild('signpost') signpost!: SignpostComponent;
 
-    selectedItemsDebug =signal<string[]>([]);
-    selectedItemsUpdate($event: string[]) {
-      this.selectedItemsDebug.set($event);
-      console.log("Selected Items Updated:", $event);
-    }
-    @ViewChild('signpost') signpost!: SignpostComponent;
+  buttonGroupItems = signal<Array<string>>([
+    'Button 1',
+    'Button 2',
+    'Button 3',
+  ]);
 
-    buttonGroupItems = signal<Array<string>>(['Button 1', 'Button 2', 'Button 3']);
+  faCheck = signal<IconDefinition>(faCircleCheck);
+  faError = signal<IconDefinition>(faCircleExclamation);
+  faInfo = signal<IconDefinition>(faInfoCircle);
+  faWarning = signal<IconDefinition>(faTriangleExclamation);
+  faTrash = signal<IconDefinition>(faTrash);
+  faExternalLink = signal<IconDefinition>(faExternalLink);
+  faCopy = signal<IconDefinition>(faCopy);
+  faTimes = signal<IconDefinition>(faTimes);
+  faPlay = signal<IconDefinition>(faPlay);
+  faStop = signal<IconDefinition>(faStop);
 
-    faCheck = signal<IconDefinition>(faCircleCheck);
-    faError = signal<IconDefinition>(faCircleExclamation);
-    faInfo = signal<IconDefinition>(faInfoCircle);
-    faWarning = signal<IconDefinition>(faTriangleExclamation);
-    faTrash = signal<IconDefinition>(faTrash);
-    faExternalLink = signal<IconDefinition>(faExternalLink);
-    faCopy = signal<IconDefinition>(faCopy);
-    faTimes = signal<IconDefinition>(faTimes);
-    faPlay = signal<IconDefinition>(faPlay);
-    faStop = signal<IconDefinition>(faStop);
+  openSignpost(event: MouseEvent) {
+    this.signpost?.show(event);
+  }
 
+  showModal = signal<boolean>(false);
 
-    openSignpost(event: MouseEvent) {
-      this.signpost?.show(event);
-    }
-
-    showModal = signal<boolean>(false);
-
-    showConfModal = signal<boolean>(false);
+  showConfModal = signal<boolean>(false);
   openModal() {
     this.showModal.set(!this.showModal());
   }
@@ -61,15 +125,15 @@ import { faPlay, faStop, faCircleCheck, faCircleExclamation, faInfoCircle, faTri
   openConfModal() {
     this.showConfModal.set(!this.showConfModal());
   }
-    //showModal = signal<boolean>(false);
+  //showModal = signal<boolean>(false);
 
-    toggleState = signal<boolean>(false);
+  toggleState = signal<boolean>(false);
 
-    activeTabIndex = signal<number>(0);
+  activeTabIndex = signal<number>(0);
 
-    inputValue = signal<string>("Initial Value");
+  inputValue = signal<string>('Initial Value');
 
-    log($event: any) {
-      console.log("(UiLibPageComponent) Event Logging", $event);
-    }
+  log($event: any) {
+    console.log('(UiLibPageComponent) Event Logging', $event);
   }
+}
