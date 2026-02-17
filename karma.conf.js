170af5dea3a3fd1d141e7aa48e274d66f788a31b
Joerg <jh.stores1@gmail.com>
2026-02-17 09:55:29 +0100

    Fix: Fix PR validation test coverage report (#32)
    * Fix coverage report path in PR validation workflow and update Karma configuration to include lcovonly reporter
    
    * remove code coverage
    
    * Enhance test coverage reporting in PR validation workflow and add Cobertura reporter in Karma configuration
    
    * Refactor test coverage reporting in PR validation workflow to simplify output handling and improve clarity

--- karma.conf.js
+++ karma.conf.js
@@ -29,7 +29,9 @@ module.exports = function (config) {
       subdir: '.',
       reporters: [
         { type: 'html' },
-        { type: 'text-summary' }
+        { type: 'text-summary' },
+        { type: 'lcovonly' },
+        { type: 'cobertura' }
       ]
     },
     reporters: ['progress', 'kjhtml'],
