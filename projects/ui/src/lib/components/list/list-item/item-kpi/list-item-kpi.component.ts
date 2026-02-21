c490b8277f66f3594b6bcef2db2fb7171d049daa
Joerg (MacAir) <jh.stores1@gmail.com>
2026-02-21 17:30:09 +0100

    Enhance: Update KPI component styles and add badge support

--- projects/ui/src/lib/components/list/list-item/item-kpi/list-item-kpi.component.ts
+++ projects/ui/src/lib/components/list/list-item/item-kpi/list-item-kpi.component.ts
@@ -4,13 +4,14 @@ import { UIBaseComponent } from '../../../../shared';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faArrowUp, faArrowDown, faMinus } from '@fortawesome/free-solid-svg-icons';
 import { ListItemKpiEntry } from '../../list.models';
+import { BadgeComponent } from "ui";
 
 
 @Component({
   selector: 'ui-list-item-kpi',
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [FontAwesomeModule, DecimalPipe, CurrencyPipe],
+  imports: [FontAwesomeModule, DecimalPipe, CurrencyPipe, BadgeComponent],
   templateUrl: './list-item-kpi.component.html',
   styleUrls: ['./list-item-kpi.component.scss']
 })
